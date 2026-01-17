925aaff823f2862b24efda2cdb8a1f2286c01181
Sviatoslav <sviatoslav@Sviatoslavs-MacBook-Air.local>
2026-01-17 16:12:53 +0200

    add solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,6 +4,6 @@ import { UserInfo } from '../UserInfo';
 export const TodoInfo = ({ todo }) => (
   <article className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}>
     <h2 className="TodoInfo__title">{todo.title}</h2>
-    <UserInfo user={todo.user ? todo.user : null} />
+    {todo.user ? <UserInfo user={todo.user} /> : null}
   </article>
 );
